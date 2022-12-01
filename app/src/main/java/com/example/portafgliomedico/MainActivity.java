66da6e588d2f14f06432aea65c33b5ed9239b857
NumenMirus <michaelbastauni@gmail.com>
2022-12-01 15:01:36 +0100

    small changes

--- app/src/main/java/com/example/portafgliomedico/MainActivity.java
+++ app/src/main/java/com/example/portafgliomedico/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "You have no memos for today!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
