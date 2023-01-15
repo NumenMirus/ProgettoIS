a711c3977f295e8439c7e99edc4d29cd4d05984c
filippo-tiozzo <filippotiozzo@hotmail.it>
2023-01-15 17:26:51 +0100

    AccountFragment changes

--- app/src/main/java/com/example/portafgliomedico/SearchFragments.java
+++ app/src/main/java/com/example/portafgliomedico/SearchFragments.java
@@ -1,18 +1,14 @@
 package com.example.portafgliomedico;
 
-import static android.content.ContentValues.TAG;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,7 +16,12 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class SearchFragments extends Fragment {
-    private Button launch_button;
+    private Button doge;
+    private Button ca_rossa;
+    private Button centrale;
+    private Button internazionale;
+    private Button menis;
+    private Button zamparo;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -70,16 +71,70 @@ public class SearchFragments extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        launch_button = getView().findViewById(R.id.search);
-        launch_button.setOnClickListener(new View.OnClickListener() {
+        //primo bottone
+        doge = getView().findViewById(R.id.doge);
+        doge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:42.4925,19.0513")); // riga probabilmente da eliminare lasciare per il momento
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.5040942,12.2529822&mode=1"));
                 intent.setPackage("com.google.android.apps.maps");
-                Intent chooser = Intent.createChooser(intent, "Launch Maps");
-                Log.d(TAG, "onClick: "+chooser.toString());
-                startActivity(chooser);
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
+            }
+        });
+        //secondo bottone
+        ca_rossa = getView().findViewById(R.id.ca_rossa);
+        ca_rossa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.5012846,12.2520203&mode=1"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
+            }
+        });
+        //terzo bottone
+        centrale = getView().findViewById(R.id.centrale);
+        centrale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.4930196,12.2381314&mode=1"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
+            }
+        });
+        //quarto bottone
+        internazionale = getView().findViewById(R.id.internazionale_salute);
+        internazionale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.4926007,12.23759&mode=1"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
+            }
+        });
+        //quinto bottone
+        menis = getView().findViewById(R.id.menis);
+        menis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.4873016,12.2389229&mode=1"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
+            }
+        });
+        //sesto bottone
+        zamparo = getView().findViewById(R.id.zamparo);
+        zamparo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("google.navigation:q=45.486659,12.242328&mode=1"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getActivity().getPackageManager())!= null)
+                    startActivity(intent);
             }
         });
     }
